7fb354742d86584428908e570bef104065934c10
tuyigi <tuyigilbert97@gmail.com>
2019-11-23 07:47:32 +0200

    creating project

---
+++ SalaryApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
